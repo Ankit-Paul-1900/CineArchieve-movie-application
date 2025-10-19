df485f92c239a4345a847d214c902f7266a2dcf7
Ankit-Paul-1900 <msclabs.work@gmail.com>
2025-10-19 23:37:36 +0530

    Update baseurl.js

--- backend/schema/baseurl.js
+++ backend/schema/baseurl.js
@@ -1,3 +1,3 @@
-const BaseUrl="http://localhost:3000";
+const BaseUrl="https://cinearchieve-movie-application-1.onrender.com"; //http://localhost:3000
 module.exports=BaseUrl;
 console.log("BaseUrl is working...");
